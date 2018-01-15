c858467c4805a6b3d3ad4c34416232139bc12744
audreylbx <audreylabuxiere@gmail.com>
2018-01-15 12:58:32 +0100

    [-] Trying to fix bug

--- api/parser/blog-article-link.js
+++ api/parser/blog-article-link.js
@@ -1,6 +1,7 @@
 import { parseLinkEntry } from './utils'
 
 const parseBlogArticleLink = entry => {
+  console.log(entry)
   const { title, description } = entry.fields
   const link = parseLinkEntry(entry.fields.link)
   return { title, description, link }
