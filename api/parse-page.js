6fbc676935d3daf0890c0a35c14cdf31b89028c3
David Miotti <david@muxumuxu.com>
2018-01-25 18:09:22 +0100

    [-] Fix crashing when draft content

--- api/parse-page.js
+++ api/parse-page.js
@@ -5,6 +5,7 @@ export default (pageEntry) => {
   const head = parseHead(pageEntry.fields.head) // Pages must have at least a 'head'
   const sectionsToParse = pageEntry.fields.sections || []
   const sections = sectionsToParse.reduce((acc, curr) => {
+    if (!curr || !curr.sys || !curr.sys.contentType || !curr.sys.contentType.sys) return acc
     const id = curr.sys.contentType.sys.id
     const componentMatched = componentsMapping.find(mapping => mapping.contentfulId === id)
     if (componentMatched) return [...acc, { id, ...componentMatched.parser(curr) }]
